e8cfaa79bb7c9cb254eb5ebe0441ae2ea06776cc
nihelouanassi <nihelouanasi@gmail.com>
2023-09-28 11:36:58 +0200

    add appbar

---
+++ lib/screens/home.dart
@@ -0,0 +1,29 @@
+import 'package:flutter/material.dart';
+
+class Home extends StatefulWidget {
+  const Home({super.key});
+  @override
+  State<Home> createState() => _HomeState();
+}
+
+class _HomeState extends State<Home> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        elevation: 0,
+        backgroundColor: Color(0xFFFAFAFA),
+        title: Center(
+            child: Text("Paris",
+                style: const TextStyle(color: Color(0xFF000000)))),
+        leading: IconButton(
+            icon: Icon(
+              Icons.menu,
+              color: Color(0xff6D7D95),
+            ),
+            onPressed: () {}),
+        actions: [IconButton(onPressed: () {}, icon: Icon(Icons.person))],
+      ),
+    );
+  }
+}
