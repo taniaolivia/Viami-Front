13a95be781b0253c38b108d0354d3ac806ea5ba9
nihelouanassi <nihelouanasi@gmail.com>
2023-09-30 11:09:34 +0200

    add menu item

--- lib/screens/home.dart
+++ lib/screens/home.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_zoom_drawer/flutter_zoom_drawer.dart';
+import 'package:viami/screens/notifications_page.dart';
 import 'package:viami/screens/secondPage.dart';
 
 import 'menu.dart';
@@ -11,9 +12,36 @@ class Home extends StatefulWidget {
 }
 
 class _HomeState extends State<Home> {
+  MenuItem cuurentItem = MenuItems.payment;
   @override
-  Widget build(BuildContext context) => ZoomDrawer(
-        mainScreen: SecondPage(),
-        menuScreen: MenuPage(),
-      );
+  Widget build(BuildContext context) {
+    return ZoomDrawer(
+        style: DrawerStyle.Style1,
+        borderRadius: 40,
+        angle: -10,
+        slideWidth: MediaQuery.of(context).size.width * 0.8,
+        showShadow: true,
+        mainScreen: getScreen(),
+        menuScreen: Builder(
+          builder: (context) => MenuPage(
+            cuurentItem: cuurentItem,
+            onSelectedItem: (item) {
+              setState(() {
+                cuurentItem = item;
+              });
+
+              ZoomDrawer.of(context)!.close();
+            },
+          ),
+        ));
+  }
+
+  Widget getScreen() {
+    switch (cuurentItem) {
+      case MenuItems.payment:
+        return SecondPage();
+      default:
+        return NotificationPage();
+    }
+  }
 }
--- lib/screens/menu.dart
+++ lib/screens/menu.dart
@@ -1,11 +1,102 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
+class MenuItems {
+  static const home = MenuItem('Home', Icons.home);
+  static const payment = MenuItem('Payment', Icons.payment);
+  static const notification = MenuItem('Notifications', Icons.notifications);
+  static const friends = MenuItem('Invite Friends', Icons.card_giftcard);
+  static const settings = MenuItem('Home', Icons.settings);
+  //static const logout = MenuItem('Logout', Icons.logout);
+
+  static const all = <MenuItem>[
+    home,
+    payment,
+    notification,
+    friends,
+    settings,
+  ];
+}
+
 class MenuPage extends StatelessWidget {
+  final MenuItem cuurentItem;
+  final ValueChanged<MenuItem> onSelectedItem;
+
+  const MenuPage(
+      {Key? key, required this.cuurentItem, required this.onSelectedItem});
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      backgroundColor: Colors.indigo,
-    );
+    return Theme(
+        data: ThemeData.dark(),
+        child: Scaffold(
+          body: Stack(
+            children: [
+              // Background Image
+              Positioned.fill(
+                child: Image.asset(
+                  'assets/drawerHeader.png', // Remplacez ceci par l'URL de votre image
+                  fit: BoxFit.cover,
+                ),
+              ),
+
+              // Menu Content
+              SafeArea(
+                child: Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: <Widget>[
+                    Spacer(),
+                    Padding(
+                      padding: EdgeInsets.only(left: 16.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(
+                            color: Color(0xFFFFFFFF),
+                            width: 3.0,
+                          ),
+                        ),
+                        child: CircleAvatar(
+                          backgroundImage: AssetImage('assets/profil.png'),
+                          radius: 50.0,
+                        ),
+                      ),
+                    ),
+                    SizedBox(
+                        height:
+                            10.0), // Add some space between CircleAvatar and Text
+                    Padding(
+                      padding: EdgeInsets.only(left: 17.0),
+                      child: Text(
+                        "Your Name", // Replace with the actual name
+                        style: TextStyle(
+                          color: Colors.white,
+                          fontSize: 19.0,
+                        ),
+                      ),
+                    ),
+                    Spacer(),
+                    ...MenuItems.all.map(buildMenuItem).toList(),
+                    Spacer(flex: 2),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ));
   }
+
+  Widget buildMenuItem(MenuItem item) => ListTile(
+        selectedTileColor: Colors.white,
+        selected: cuurentItem == item,
+        minLeadingWidth: 20,
+        leading: Icon(item.icon),
+        title: Text(item.title),
+        onTap: () => onSelectedItem(item),
+      );
+}
+
+class MenuItem {
+  final String title;
+  final IconData icon;
+
+  const MenuItem(this.title, this.icon);
 }
