226dfee74cdfa408852750203466e66a63bd0289
Tania Olivia <tania.olivia69@yahoo.com>
2023-10-18 14:50:48 +0200

    display user's data in profile first version

--- lib/screens/profile_page.dart
+++ lib/screens/profile_page.dart
@@ -1,18 +1,43 @@
 import 'package:flutter/material.dart';
-import 'package:flutter/widgets.dart';
-
-import '../components/NavigationBarComponent.dart';
-import '../widgets/menu_widget.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:viami/models-api/user.dart';
+import 'package:viami/services/user.service.dart';
 
 class ProfilePage extends StatelessWidget {
+  final storage = FlutterSecureStorage();
+
+  String? token = "";
+  String? userId = "";
+
+  Future<User> getUser() {
+    Future<User> getConnectedUser() async {
+      token = await storage.read(key: "token");
+      userId = await storage.read(key: "userId");
+
+      return UserService().getUserById(userId.toString(), token.toString());
+    }
+
+    return getConnectedUser();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: Colors.blue,
-      body: Text(
-        "ProfilePage",
-        style: TextStyle(color: Colors.black12),
-      ),
-    );
+        body: FutureBuilder<User>(
+            future: getUser(),
+            builder: (context, snapshot) {
+              if (snapshot.hasData) {
+                var user = snapshot.data!;
+                return Column(
+                  children: <Widget>[
+                    const Text("ProfilePage",
+                        style: TextStyle(color: Colors.black12)),
+                    Text("Name : ${user.firstName}")
+                  ],
+                );
+              } else {}
+
+              return const CircularProgressIndicator();
+            }));
   }
 }
