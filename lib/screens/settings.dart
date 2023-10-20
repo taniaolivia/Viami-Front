d9167b65f59b7fedc4406ec2db42a1b9dfc71115
nihelouanassi <nihelouanasi@gmail.com>
2023-10-20 11:42:58 +0200

    add getUser to settings page

--- lib/screens/settings.dart
+++ lib/screens/settings.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
+import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 
+import '../models-api/user.dart';
 import '../services/user.service.dart';
 
 class SettingsPage extends StatefulWidget {
@@ -10,6 +12,22 @@ class SettingsPage extends StatefulWidget {
 }
 
 class _SettingsPage extends State<SettingsPage> {
+  final storage = const FlutterSecureStorage();
+
+  String? token = "";
+  String? userId = "";
+
+  Future<User> getUser() {
+    Future<User> getConnectedUser() async {
+      token = await storage.read(key: "token");
+      userId = await storage.read(key: "userId");
+
+      return UserService().getUserById(userId.toString(), token.toString());
+    }
+
+    return getConnectedUser();
+  }
+
   bool startAnimation = false;
   final List<String> items = [
     "Supprimer le compte ",
