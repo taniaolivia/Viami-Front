b0802cd985bb4192fdc160e27d8509b3c781f1ce
Tania Olivia <tania.olivia69@yahoo.com>
2023-10-22 08:17:25 +0200

    fix error when add new image to user's data

--- lib/services/userImage/userImage.service.dart
+++ lib/services/userImage/userImage.service.dart
@@ -4,18 +4,17 @@ import 'package:flutter_dotenv/flutter_dotenv.dart';
 
 class UserImageService {
   Future<Map<String, dynamic>> addUserImage(
-      String userId, int imageId, String token) async {
+      String userId, String imagePath, String token) async {
     final response = await http.post(
         Uri.parse("${dotenv.env['API_URL']}/users/$userId/images"),
         headers: <String, String>{
           "Content-Type": "application/json",
           'Authorization': token
         },
-        body: jsonEncode(<String, dynamic>{"imageId": imageId}));
+        body: jsonEncode(<String, dynamic>{"image": imagePath}));
 
     if (response.statusCode == 200) {
       var res = json.decode(response.body);
-
       return res;
     } else {
       throw Exception("Failed to load user");
