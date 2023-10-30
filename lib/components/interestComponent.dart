cd3a7190cf7c964e66c034ab9534f973009fa40e
Tania Olivia <tania.olivia69@yahoo.com>
2023-10-30 01:05:38 +0100

    set the add button to top right position

--- lib/components/interestComponent.dart
+++ lib/components/interestComponent.dart
@@ -2,38 +2,43 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:viami/components/interestList.dart';
-import 'package:viami/models-api/user/user.dart';
 import 'package:viami/models-api/userInterest/usersInterests.dart';
-import 'package:viami/services/user/user.service.dart';
 import 'package:viami/services/userInterest/usersInterests.service.dart';
 
-class InterestComponent extends StatelessWidget {
-  InterestComponent({super.key});
+class InterestComponent extends StatefulWidget {
+  final String page;
+  final String userId;
 
+  const InterestComponent({Key? key, required this.page, required this.userId})
+      : super(key: key);
+
+  @override
+  State<InterestComponent> createState() => _InterestComponentState();
+}
+
+class _InterestComponentState extends State<InterestComponent> {
   final storage = const FlutterSecureStorage();
 
   String? token = "";
-  String? userId = "";
   int? userInterestsLength = 0;
   List<UserInterest>? userInterests = [];
 
-  Future<UsersInterests> getUserInterests() {
-    Future<UsersInterests> getAllInterests() async {
-      token = await storage.read(key: "token");
-      userId = await storage.read(key: "userId");
-
-      return UsersInterestsService()
-          .getUserInterestsById(userId.toString(), token.toString());
-    }
-
-    return getAllInterests();
-  }
-
   @override
   Widget build(BuildContext context) {
+    Future<UsersInterests> getUserInterests() {
+      Future<UsersInterests> getAllInterests() async {
+        token = await storage.read(key: "token");
+
+        return UsersInterestsService()
+            .getUserInterestsById(widget.userId, token.toString());
+      }
+
+      return getAllInterests();
+    }
+
     return Column(
       children: <Widget>[
-        Row(children: [
+        Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
           const Align(
             alignment: Alignment.topLeft,
             child: AutoSizeText(
@@ -44,16 +49,17 @@ class InterestComponent extends StatelessWidget {
                   TextStyle(color: Colors.black, fontWeight: FontWeight.w700),
             ),
           ),
-          GestureDetector(
-              onTap: () {
-                Navigator.push(
-                  context,
-                  MaterialPageRoute(builder: (context) => const InterestList()),
-                );
-              },
-              child: Container(
-                  width: MediaQuery.of(context).size.width / 2.1,
-                  child: const AutoSizeText(
+          widget.page == "edit"
+              ? GestureDetector(
+                  onTap: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(
+                          builder: (context) => const InterestList()),
+                    );
+                  },
+                  child: Container(
+                      child: const AutoSizeText(
                     "< Ajouter",
                     style: TextStyle(
                       color: Colors.blue,
@@ -63,22 +69,72 @@ class InterestComponent extends StatelessWidget {
                     maxFontSize: 13,
                     textAlign: TextAlign.right,
                   )))
+              : Container(),
         ]),
         const SizedBox(height: 10),
-        GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const InterestList()),
-              );
-            },
-            child: Container(
+        widget.page == "edit"
+            ? GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const InterestList()),
+                  );
+                },
+                child: Container(
+                    width: MediaQuery.of(context).size.width,
+                    constraints: const BoxConstraints(
+                        minHeight: 45, maxHeight: double.infinity),
+                    decoration: const BoxDecoration(
+                        color: Color(0xFFF4F4F4),
+                        borderRadius: BorderRadius.all(Radius.circular(5))),
+                    child: FutureBuilder<UsersInterests>(
+                        future: getUserInterests(),
+                        builder: (context, snapshot) {
+                          if (snapshot.hasData) {
+                            var data = snapshot.data!;
+
+                            userInterestsLength = data.userInterests.length;
+                            userInterests = data.userInterests;
+
+                            return Wrap(
+                                alignment: WrapAlignment.start,
+                                spacing: 0.0,
+                                runSpacing: 0.0,
+                                children: List.generate(
+                                    data.userInterests.length, (index) {
+                                  return Container(
+                                      padding: const EdgeInsets.fromLTRB(
+                                          20, 10, 20, 10),
+                                      constraints: const BoxConstraints(
+                                          maxWidth: double.infinity),
+                                      decoration: BoxDecoration(
+                                        color: Colors.white,
+                                        border: Border.all(
+                                            color: const Color(0xFF0081CF)),
+                                        borderRadius:
+                                            BorderRadius.circular(10.0),
+                                      ),
+                                      child: AutoSizeText(
+                                        data.userInterests[index].interest,
+                                        style: const TextStyle(
+                                          color: Color(0xFF0081CF),
+                                          fontWeight: FontWeight.w600,
+                                        ),
+                                        minFontSize: 10,
+                                        maxFontSize: 12,
+                                      ));
+                                }).toList());
+                          }
+
+                          return const Align(
+                              alignment: Alignment.center,
+                              child: CircularProgressIndicator());
+                        })))
+            : Container(
                 width: MediaQuery.of(context).size.width,
                 constraints: const BoxConstraints(
                     minHeight: 45, maxHeight: double.infinity),
-                decoration: const BoxDecoration(
-                    color: Color(0xFFF4F4F4),
-                    borderRadius: BorderRadius.all(Radius.circular(5))),
                 child: FutureBuilder<UsersInterests>(
                     future: getUserInterests(),
                     builder: (context, snapshot) {
@@ -100,15 +156,15 @@ class InterestComponent extends StatelessWidget {
                                   constraints: const BoxConstraints(
                                       maxWidth: double.infinity),
                                   decoration: BoxDecoration(
-                                    color: const Color(0xFFFFDAA2),
+                                    color: Colors.white,
                                     border: Border.all(
-                                        color: const Color(0xFFE85124)),
-                                    borderRadius: BorderRadius.circular(50.0),
+                                        color: const Color(0xFF0081CF)),
+                                    borderRadius: BorderRadius.circular(10.0),
                                   ),
                                   child: AutoSizeText(
                                     data.userInterests[index].interest,
                                     style: const TextStyle(
-                                      color: Colors.black,
+                                      color: Color(0xFF0081CF),
                                       fontWeight: FontWeight.w600,
                                     ),
                                     minFontSize: 10,
@@ -120,7 +176,7 @@ class InterestComponent extends StatelessWidget {
                       return const Align(
                           alignment: Alignment.center,
                           child: CircularProgressIndicator());
-                    }))),
+                    })),
         const SizedBox(height: 30),
       ],
     );
--- lib/components/languageComponent.dart
+++ lib/components/languageComponent.dart
@@ -2,13 +2,15 @@ import 'package:auto_size_text/auto_size_text.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
 import 'package:viami/components/languageList.dart';
-import 'package:viami/models-api/user/user.dart';
 import 'package:viami/models-api/userLanguage/usersLanguages.dart';
-import 'package:viami/services/user/user.service.dart';
 import 'package:viami/services/userLanguage/usersLanguages.service.dart';
 
 class LanguageComponent extends StatefulWidget {
-  const LanguageComponent({Key? key}) : super(key: key);
+  final String page;
+  final String userId;
+
+  const LanguageComponent({Key? key, required this.page, required this.userId})
+      : super(key: key);
 
   @override
   State<LanguageComponent> createState() => _LanguageComponentState();
@@ -44,26 +46,27 @@ class _LanguageComponentState extends State<LanguageComponent> {
   @override
   Widget build(BuildContext context) {
     return Column(children: <Widget>[
-      Row(children: [
+      Row(mainAxisAlignment: MainAxisAlignment.spaceBetween, children: [
         const Align(
           alignment: Alignment.topLeft,
           child: AutoSizeText(
-            "Mes langues",
+            "Les langues que je maîtrise",
             minFontSize: 11,
             maxFontSize: 13,
             style: TextStyle(color: Colors.black, fontWeight: FontWeight.w700),
           ),
         ),
-        GestureDetector(
-            onTap: () {
-              Navigator.push(
-                context,
-                MaterialPageRoute(builder: (context) => const LanguageList()),
-              );
-            },
-            child: Container(
-                width: MediaQuery.of(context).size.width / 1.55,
-                child: const AutoSizeText(
+        widget.page == "edit"
+            ? GestureDetector(
+                onTap: () {
+                  Navigator.push(
+                    context,
+                    MaterialPageRoute(
+                        builder: (context) => const LanguageList()),
+                  );
+                },
+                child: Container(
+                    child: const AutoSizeText(
                   "< Ajouter",
                   style: TextStyle(
                     color: Colors.blue,
@@ -73,65 +76,114 @@ class _LanguageComponentState extends State<LanguageComponent> {
                   maxFontSize: 13,
                   textAlign: TextAlign.right,
                 )))
+            : Container()
       ]),
       const SizedBox(height: 10),
-      GestureDetector(
-          onTap: () {
-            Navigator.push(
-              context,
-              MaterialPageRoute(builder: (context) => const LanguageList()),
-            );
-          },
-          child: Container(
-            width: MediaQuery.of(context).size.width,
-            constraints:
-                const BoxConstraints(minHeight: 45, maxHeight: double.infinity),
-            decoration: const BoxDecoration(
-                color: Color(0xFFF4F4F4),
-                borderRadius: BorderRadius.all(Radius.circular(5))),
-            child: FutureBuilder<UsersLanguages>(
-                future: getUserLanguages(),
-                builder: (context, snapshot) {
-                  if (snapshot.hasData) {
-                    var data = snapshot.data!;
+      widget.page == "edit"
+          ? GestureDetector(
+              onTap: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => const LanguageList()),
+                );
+              },
+              child: Container(
+                width: MediaQuery.of(context).size.width,
+                constraints: const BoxConstraints(
+                    minHeight: 45, maxHeight: double.infinity),
+                decoration: const BoxDecoration(
+                    color: Color(0xFFF4F4F4),
+                    borderRadius: BorderRadius.all(Radius.circular(5))),
+                child: FutureBuilder<UsersLanguages>(
+                    future: getUserLanguages(),
+                    builder: (context, snapshot) {
+                      if (snapshot.hasData) {
+                        var data = snapshot.data!;
 
-                    userLanguagesLength = data.userLanguages.length;
-                    userLanguages = data.userLanguages;
+                        userLanguagesLength = data.userLanguages.length;
+                        userLanguages = data.userLanguages;
 
-                    return Wrap(
-                        alignment: WrapAlignment.start,
-                        spacing: 0.0,
-                        runSpacing: 0.0,
-                        children:
-                            List.generate(data.userLanguages.length, (index) {
-                          return Container(
-                              padding:
-                                  const EdgeInsets.fromLTRB(20, 10, 20, 10),
-                              constraints: const BoxConstraints(
-                                  maxWidth: double.infinity),
-                              decoration: BoxDecoration(
-                                color: const Color(0xFFDFFCDE),
-                                border:
-                                    Border.all(color: const Color(0xFF00611B)),
-                                borderRadius: BorderRadius.circular(50.0),
-                              ),
-                              child: AutoSizeText(
-                                data.userLanguages[index].language,
-                                style: const TextStyle(
-                                  color: Colors.black,
-                                  fontWeight: FontWeight.w600,
+                        return Wrap(
+                            alignment: WrapAlignment.start,
+                            spacing: 0.0,
+                            runSpacing: 0.0,
+                            children: List.generate(data.userLanguages.length,
+                                (index) {
+                              return Container(
+                                  padding:
+                                      const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                                  constraints: const BoxConstraints(
+                                      maxWidth: double.infinity),
+                                  decoration: BoxDecoration(
+                                    color: Colors.white,
+                                    border: Border.all(
+                                        color: const Color(0xFF0081CF)),
+                                    borderRadius: BorderRadius.circular(10.0),
+                                  ),
+                                  child: AutoSizeText(
+                                    data.userLanguages[index].language,
+                                    style: const TextStyle(
+                                      color: Color(0xFF0081CF),
+                                      fontWeight: FontWeight.w600,
+                                    ),
+                                    minFontSize: 10,
+                                    maxFontSize: 12,
+                                  ));
+                            }).toList());
+                      }
+
+                      return const Align(
+                          alignment: Alignment.center,
+                          child: CircularProgressIndicator());
+                    }),
+              ))
+          : Container(
+              width: MediaQuery.of(context).size.width,
+              constraints: const BoxConstraints(
+                  minHeight: 45, maxHeight: double.infinity),
+              child: FutureBuilder<UsersLanguages>(
+                  future: getUserLanguages(),
+                  builder: (context, snapshot) {
+                    if (snapshot.hasData) {
+                      var data = snapshot.data!;
+
+                      userLanguagesLength = data.userLanguages.length;
+                      userLanguages = data.userLanguages;
+
+                      return Wrap(
+                          alignment: WrapAlignment.start,
+                          spacing: 0.0,
+                          runSpacing: 0.0,
+                          children:
+                              List.generate(data.userLanguages.length, (index) {
+                            return Container(
+                                padding:
+                                    const EdgeInsets.fromLTRB(20, 10, 20, 10),
+                                constraints: const BoxConstraints(
+                                    maxWidth: double.infinity),
+                                decoration: BoxDecoration(
+                                  color: Colors.white,
+                                  border: Border.all(
+                                      color: const Color(0xFF0081CF)),
+                                  borderRadius: BorderRadius.circular(10.0),
                                 ),
-                                minFontSize: 10,
-                                maxFontSize: 12,
-                              ));
-                        }).toList());
-                  }
+                                child: AutoSizeText(
+                                  data.userLanguages[index].language,
+                                  style: const TextStyle(
+                                    color: Color(0xFF0081CF),
+                                    fontWeight: FontWeight.w600,
+                                  ),
+                                  minFontSize: 10,
+                                  maxFontSize: 12,
+                                ));
+                          }).toList());
+                    }
 
-                  return const Align(
-                      alignment: Alignment.center,
-                      child: CircularProgressIndicator());
-                }),
-          )),
+                    return const Align(
+                        alignment: Alignment.center,
+                        child: CircularProgressIndicator());
+                  }),
+            ),
       const SizedBox(height: 40)
     ]);
   }
