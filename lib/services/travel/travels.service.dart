daedae71153898a90a079d440814b6e30995bfab
Tania Olivia <tania.olivia69@yahoo.com>
2023-11-08 04:11:28 +0100

    add travels service to get list of travels

---
+++ lib/services/travel/travels.service.dart
@@ -0,0 +1,24 @@
+import 'package:http/http.dart' as http;
+import 'dart:convert';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
+import 'package:viami/models-api/interest/interests.dart';
+import 'package:viami/models-api/travel/travels.dart';
+
+class TravelsService {
+  Future<Travels> getAllTravels(String token) async {
+    final response = await http.get(
+      Uri.parse('${dotenv.env['API_URL']}/travels'),
+      headers: <String, String>{
+        'Authorization': token,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      var res = json.decode(response.body);
+
+      return Travels.fromJson(res);
+    } else {
+      throw Exception('Failed to load travels');
+    }
+  }
+}
