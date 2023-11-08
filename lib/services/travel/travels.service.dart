a75d654433dedd229adff4c0a870fd3045c066fb
Tania Olivia <tania.olivia69@yahoo.com>
2023-11-08 22:40:13 +0100

    add function to search trips

--- lib/services/travel/travels.service.dart
+++ lib/services/travel/travels.service.dart
@@ -21,4 +21,21 @@ class TravelsService {
       throw Exception('Failed to load travels');
     }
   }
+
+  Future<Travels> searchTravels(String token, String location) async {
+    final response = await http.get(
+      Uri.parse('${dotenv.env['API_URL']}/search/travels?location=${location}'),
+      headers: <String, String>{
+        'Authorization': token,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      var res = json.decode(response.body);
+
+      return Travels.fromJson(res);
+    } else {
+      throw Exception('Failed to load travels');
+    }
+  }
 }
