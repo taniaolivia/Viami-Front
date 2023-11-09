84f19391daaadd312312c15a0f42667c18bb68e4
nihelouanassi <nihelouanasi@gmail.com>
2023-11-09 21:12:20 +0100

    add recommended travel service

--- lib/services/travel/recommendedTravel.service.dart
+++ lib/services/travel/recommendedTravel.service.dart
@@ -3,6 +3,7 @@ import 'package:http/http.dart' as http;
 
 import 'package:flutter_dotenv/flutter_dotenv.dart';
 
+import '../../models-api/travel/travel.dart';
 import '../../models-api/travel/travels.dart';
 
 class RecommendedTravelsService {
@@ -23,6 +24,24 @@ class RecommendedTravelsService {
       return Travels.fromJson(res);
     } else {
       throw Exception('Failed to load Recommended travels');
+    }
+  }
+
+  Future<Travel> getRecommendTravelById(String travelId, String token) async {
+    final response = await http.get(
+      Uri.parse('${dotenv.env['API_URL']}/recommend/travels/$travelId'),
+      headers: <String, String>{
+        'Authorization': token,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      var res = json.decode(response.body);
+      print(res['travel']);
+
+      return Travel.fromJson(res['travel']);
+    } else {
+      throw Exception('Failed to load travel');
     }
   }
 }
