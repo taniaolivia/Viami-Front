d13d106413d9a8f8e57a3720a2202691e8e5a4aa
Tania Olivia <tania.olivia69@yahoo.com>
2023-11-10 13:55:55 +0100

    add fade transition page component

---
+++ lib/components/pageTransition.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+
+class FadePageRoute<T> extends PageRouteBuilder<T> {
+  final Widget page;
+
+  FadePageRoute({required this.page})
+      : super(
+          pageBuilder: (context, animation, secondaryAnimation) => page,
+          transitionsBuilder: (context, animation, secondaryAnimation, child) {
+            const begin = 0.0;
+            const end = 1.0;
+            const curve = Curves.easeInOut;
+
+            var tween = Tween(begin: begin, end: end).chain(
+              CurveTween(curve: curve),
+            );
+
+            var fadeAnimation = animation.drive(tween);
+
+            return FadeTransition(opacity: fadeAnimation, child: child);
+          },
+        );
+}
