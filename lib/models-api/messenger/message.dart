cfaaf47f29a0e17ff8b962f8fa27881abe571faa
Tania Olivia <tania.olivia69@yahoo.com>
2023-11-16 16:10:16 +0100

    add functions to get the user's messenger

--- lib/models-api/messenger/message.dart
+++ lib/models-api/messenger/message.dart
@@ -10,45 +10,24 @@ class Message {
       required this.responderId,
       required this.message,
       required this.date,
-      required this.firstName,
-      required this.lastName,
-      required this.email,
-      required this.password,
-      this.description,
-      required this.location,
-      required this.phoneNumber,
-      required this.birthday,
-      this.age,
-      required this.sex,
-      this.lastConnection,
-      this.connected,
-      this.profileImage,
-      this.verifyEmailToken,
-      this.emailVerified,
-      required this.plan});
+      required this.senderFirstName,
+      required this.senderLastName,
+      required this.responderFirstName,
+      required this.responderLastName,
+      required this.read
+     });
 
   factory Message.fromJson(Map<String?, dynamic> json) => _$MessageFromJson(json);
   Map<String?, dynamic> toJson() => _$MessageToJson(this);
 
-  final String? id;
+  final int id;
   final String senderId;
   final String responderId;
-  final String firstName;
-  final String lastName;
-  final String email;
-  final String password;
-  final String? description;
-  final String location;
-  final String phoneNumber;
-  final String birthday;
-  final num? age;
-  final String sex;
-  final String? lastConnection;
-  final String? message;
-  final String? connected;
-  final String? profileImage;
-  final String? verifyEmailToken;
-  final String? emailVerified;
-  final String plan;
+  final String senderFirstName;
+  final String senderLastName;
+  final String responderFirstName;
+  final String responderLastName;
+  final String message;
   final String date;
+  final String read;
 }
