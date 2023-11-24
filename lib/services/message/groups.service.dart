06c7b030f0333a53223037b10721d7d91f7f4048
nihelouanassi <nihelouanasi@gmail.com>
2023-11-24 03:17:11 +0100

    add service to get discussion by filter

--- lib/services/message/groups.service.dart
+++ lib/services/message/groups.service.dart
@@ -26,4 +26,46 @@ class GroupsService {
       throw Exception('Failed to load travels');
     }
   }
+
+  Future<Groups> getTwoUserDiscussions(String token, String userId) async {
+    final response = await http.get(
+      Uri.parse('${dotenv.env['API_URL']}/discussions/twoUsers/$userId'),
+      headers: <String, String>{
+        'Authorization': token,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      var res = json.decode(response.body);
+      print("ressssssssssssss two user ");
+      print(res);
+      print("resss discusionssss two user");
+      print(res["discussions"]);
+
+      return Groups.fromJson(res);
+    } else {
+      throw Exception('Failed to load travels');
+    }
+  }
+
+  Future<Groups> getGroupUsersDiscussions(String token, String userId) async {
+    final response = await http.get(
+      Uri.parse('${dotenv.env['API_URL']}/discussions/groupUsers/$userId'),
+      headers: <String, String>{
+        'Authorization': token,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      var res = json.decode(response.body);
+      print("ressssssssssssss  goupppp");
+      print(res);
+      print("resss discusionssss goupppp ");
+      print(res["discussions"]);
+
+      return Groups.fromJson(res);
+    } else {
+      throw Exception('Failed to load travels');
+    }
+  }
 }
