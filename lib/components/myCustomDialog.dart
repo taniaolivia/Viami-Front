0d4e9e0ed6a69a2db82e32c24f924482a0188b93
nihelouanassi <nihelouanasi@gmail.com>
2023-12-01 19:11:37 +0100

    add alertdialog to display users

--- lib/components/myCustomDialog.dart
+++ lib/components/myCustomDialog.dart
@@ -1,11 +1,19 @@
+import 'dart:ui';
+
 import 'package:flutter/material.dart';
+import 'package:flutter_dotenv/flutter_dotenv.dart';
 import 'package:flutter_secure_storage/flutter_secure_storage.dart';
+import 'package:viami/models-api/messenger/message.dart';
 
 import '../models-api/user/users.dart';
+import '../models-api/userImage/usersImages.dart';
+import '../services/message/groups.service.dart';
 import '../services/user/users.service.dart';
+import '../services/userImage/usersImages.service.dart';
 
 class MyCustomDialog extends StatefulWidget {
-  const MyCustomDialog({Key? key}) : super(key: key);
+  final int? groupId;
+  const MyCustomDialog({Key? key, this.groupId}) : super(key: key);
 
   @override
   State<MyCustomDialog> createState() => _MyCustomDialogState();
@@ -17,14 +25,34 @@ class _MyCustomDialogState extends State<MyCustomDialog> {
   final storage = const FlutterSecureStorage();
   String? token = "";
   Users? allUsers;
+  String? userId = "";
 
-  Future<Users> getAll() async {
+  Future<void> getAll() async {
     token = await storage.read(key: "token");
-    final allUsers = await UsersService().getAllUsers(token.toString());
-    print("allllllllllllllusers");
-    print(allUsers.users);
+    userId = await storage.read(key: "userId");
+    allUsers = await UsersService()
+        .getUsersWithConversation(token.toString(), userId!);
 
-    return allUsers;
+    if (mounted) {
+      setState(() {});
+    }
+  }
+
+  Future<UsersImages> getUserImages(String userId) async {
+    token = await storage.read(key: "token");
+
+    final images = await UsersImagesService().getUserImagesById(
+      userId,
+      token.toString(),
+    );
+
+    return images;
+  }
+
+  void search(Users users) {
+    setState(() {
+      allUsers = users;
+    });
   }
 
   @override
@@ -35,137 +63,213 @@ class _MyCustomDialogState extends State<MyCustomDialog> {
 
   @override
   Widget build(BuildContext context) {
-    return FutureBuilder<Users>(
-      future: getAll(),
-      builder: (context, snapshot) {
-        if (snapshot.connectionState == ConnectionState.waiting) {
-          return Center(child: CircularProgressIndicator());
-        } else if (snapshot.hasError) {
-          return AlertDialog(
-            title: const Text('Erreur'),
-            content: const Text('Erreur lors de la récupération des données'),
-          );
-        } else {
-          Users allUsers = snapshot.data!;
-          print("allllllllllllllll");
-          print(allUsers);
-          print(allUsers.users);
-          print(allUsers.users[0].firstName);
-
-          return AlertDialog(
-            title: const Text('Sélectionnez un voyageur'),
-            content: Column(
-              children: [
-                // Barre de recherche
-                Row(
-                  children: [
-                    Container(
-                      width: MediaQuery.of(context).size.width / 1.5,
-                      child: Form(
-                        key: _formKeySearchNameUser,
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.center,
-                          children: <Widget>[
-                            TextFormField(
-                              validator: (value) {
-                                if (value == null || value.isEmpty) {
-                                  return 'Veuillez remplir un prénom';
-                                }
-                                return null;
-                              },
-                              controller: searchControllerNameUser,
-                              decoration: const InputDecoration(
-                                border: OutlineInputBorder(
-                                  borderRadius:
-                                      BorderRadius.all(Radius.circular(15)),
-                                ),
-                                contentPadding:
-                                    EdgeInsets.fromLTRB(15, 5, 10, 5),
-                                labelText: 'Recherche par prénom',
-                                hintText: '',
-                                labelStyle: TextStyle(fontSize: 12),
-                              ),
-                            ),
-                          ],
-                        ),
-                      ),
-                    ),
-                    const SizedBox(
-                      width: 10,
-                    ),
-                    ElevatedButton(
-                      style: ElevatedButton.styleFrom(
-                        backgroundColor: const Color(0xFF0081CF),
-                        padding: const EdgeInsets.only(top: 12, bottom: 12),
-                        shape: const RoundedRectangleBorder(
-                          borderRadius: BorderRadius.all(Radius.circular(15)),
-                        ),
-                      ),
-                      child: const Icon(
-                        Icons.search,
-                        color: Colors.white,
-                        size: 25.0,
-                      ),
-                      onPressed: () async {
-                        if (_formKeySearchNameUser.currentState!.validate()) {
-                          var searchName = searchControllerNameUser.text;
-
-                          var userSearch = await UsersService()
-                              .getAllUsersBySerch(token!, searchName);
-
-                          if (userSearch != null) {
-                            setState(() {
-                              allUsers = userSearch;
-                            });
+    return AlertDialog(
+      title: const Text('Sélectionnez un voyageur'),
+      content: Column(
+        children: [
+          // Barre de recherche
+          Row(
+            children: [
+              Container(
+                width: MediaQuery.of(context).size.width / 2.2,
+                child: Form(
+                  key: _formKeySearchNameUser,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: <Widget>[
+                      TextFormField(
+                        validator: (value) {
+                          if (value == null || value.isEmpty) {
+                            return 'Veuillez remplir un prénom';
                           }
-
-                          FocusScope.of(context).unfocus();
-                        }
-                      },
-                    ),
-                  ],
-                ),
-                Align(
-                  alignment: Alignment.centerLeft,
-                  child: TextButton(
-                    onPressed: () async {
-                      var clearName = await getAll();
-
-                      setState(() {
-                        allUsers = clearName;
-                        searchControllerNameUser.text = "";
-                      });
-
-                      FocusScope.of(context).unfocus();
-                    },
-                    child: const Text(
-                      "Réinitialiser",
-                      style: TextStyle(fontSize: 12),
-                    ),
-                  ),
-                ),
-                Expanded(
-                  child: ListView.builder(
-                    itemCount: allUsers.users.length,
-                    itemBuilder: (context, index) {
-                      var user = allUsers.users[index];
-                      print("userrrrrrrrrr");
-                      print(user.firstName ?? "");
-
-                      return ListTile(
-                        title: Text(user.firstName ?? ""),
-                        onTap: () {
-                          Navigator.pop(context);
+                          return null;
                         },
-                      );
-                    },
+                        controller: searchControllerNameUser,
+                        decoration: const InputDecoration(
+                          border: OutlineInputBorder(
+                            borderRadius: BorderRadius.all(Radius.circular(15)),
+                          ),
+                          contentPadding: EdgeInsets.fromLTRB(15, 5, 10, 5),
+                          labelText: 'Recherche par prénom',
+                          hintText: '',
+                          labelStyle: TextStyle(fontSize: 12),
+                        ),
+                      ),
+                    ],
                   ),
-                )
-              ],
+                ),
+              ),
+              const SizedBox(
+                width: 5,
+              ),
+              ElevatedButton(
+                style: ElevatedButton.styleFrom(
+                  backgroundColor: const Color(0xFF0081CF),
+                  padding: const EdgeInsets.only(top: 20, bottom: 20),
+                  shape: const RoundedRectangleBorder(
+                    borderRadius: BorderRadius.all(Radius.circular(15)),
+                  ),
+                ),
+                child: const Icon(
+                  Icons.search,
+                  color: Colors.white,
+                  size: 20.0,
+                ),
+                onPressed: () async {
+                  if (_formKeySearchNameUser.currentState!.validate()) {
+                    var searchName = searchControllerNameUser.text;
+
+                    var userSearch = await UsersService()
+                        .getAllUsersBySerch(token!, searchName);
+
+                    if (userSearch != null) {
+                      setState(() {
+                        allUsers = userSearch;
+                      });
+                      print("serchusers");
+                      print(allUsers?.users.length);
+                    }
+
+                    FocusScope.of(context).unfocus();
+                    setState(() {});
+                  }
+                },
+              ),
+            ],
+          ),
+          Align(
+            alignment: Alignment.centerLeft,
+            child: TextButton(
+              onPressed: () async {
+                var clearName = await UsersService()
+                    .getUsersWithConversation(token.toString(), userId!);
+
+                setState(() {
+                  allUsers = clearName;
+                  searchControllerNameUser.text = "";
+                });
+
+                FocusScope.of(context).unfocus();
+              },
+              child: const Text(
+                "Réinitialiser",
+                style: TextStyle(fontSize: 12),
+              ),
             ),
-          );
-        }
-      },
+          ),
+          if (allUsers?.users.isEmpty ?? true) Text('Aucun utilisateur trouvé'),
+          if (!(allUsers?.users.isEmpty ?? true))
+            Column(
+              mainAxisAlignment: MainAxisAlignment.start,
+              children: List.generate(allUsers?.users.length ?? 0, (index) {
+                var user = allUsers?.users[index];
+
+                return Padding(
+                  padding: const EdgeInsets.symmetric(vertical: 8),
+                  child: Row(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      Row(children: [
+                        FutureBuilder<UsersImages>(
+                          future: getUserImages(user!.id ?? ""),
+                          builder: (context, snapshot) {
+                            if (snapshot.connectionState ==
+                                ConnectionState.waiting) {
+                              return Container();
+                            } else if (snapshot.hasError) {
+                              return Text('Error: ${snapshot.error}');
+                            } else if (!snapshot.hasData) {
+                              return const Text('');
+                            }
+
+                            var image = snapshot.data!;
+                            var avatar = image.userImages.length != 0
+                                ? CircleAvatar(
+                                    backgroundImage: NetworkImage(
+                                        "${image.userImages[0].image}"),
+                                    maxRadius: 25,
+                                  )
+                                : CircleAvatar(
+                                    backgroundColor: const Color.fromARGB(
+                                        255, 220, 234, 250),
+                                    foregroundImage: NetworkImage(
+                                        "${dotenv.env['CDN_URL']}/assets/noprofile.png"),
+                                    maxRadius: 25,
+                                  );
+
+                            return avatar;
+                          },
+                        ),
+                        const SizedBox(width: 8),
+                        Text('${user?.firstName} ${user?.lastName}')
+                      ]),
+                      IconButton(
+                        style: ButtonStyle(
+                            backgroundColor: MaterialStateProperty.all(
+                          Colors.blue,
+                        )),
+                        onPressed: () async {
+                          try {
+                            var result = await GroupsService().addUserToGroup(
+                                token.toString(),
+                                user?.id,
+                                widget.groupId.toString());
+                            var message = result.toString();
+                            print("messagee");
+                            print(result.toString());
+
+                            // Show success dialog
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  content: Text(message),
+                                  actions: <Widget>[
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                        Navigator.of(context).popAndPushNamed(
+                                            '/messages'); // Refresh the page
+                                      },
+                                      child: const Text('OK'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          } catch (e) {
+                            showDialog(
+                              context: context,
+                              builder: (BuildContext context) {
+                                return AlertDialog(
+                                  title: Text('Error'),
+                                  content: Text(e.toString()),
+                                  actions: <Widget>[
+                                    TextButton(
+                                      onPressed: () {
+                                        Navigator.of(context).pop();
+                                      },
+                                      child: Text('OK'),
+                                    ),
+                                  ],
+                                );
+                              },
+                            );
+                          }
+                        },
+                        icon: Icon(
+                          Icons.add,
+                          size: 25,
+                          color: Colors.red,
+                        ),
+                      ),
+                    ],
+                  ),
+                );
+              }),
+            ),
+        ],
+      ),
     );
   }
 }
