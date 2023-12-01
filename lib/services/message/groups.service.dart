b0f0d61e5c3fa0ef5a679575df91caebb1d7be84
nihelouanassi <nihelouanasi@gmail.com>
2023-12-01 19:10:39 +0100

    add user into a group service

--- lib/services/message/groups.service.dart
+++ lib/services/message/groups.service.dart
@@ -93,4 +93,28 @@ class GroupsService {
       throw Exception('Failed to load messages');
     }
   }
+
+  Future<String> addUserToGroup(
+      String token, String? userToAddId, String groupId) async {
+    final response = await http.post(
+        Uri.parse(
+            '${dotenv.env['API_URL']}/messages/addUserToGroup/$userToAddId/$groupId'),
+        headers: <String, String>{
+          'Authorization': token,
+        });
+
+    if (response.statusCode == 200) {
+      var res = json.decode(response.body);
+      var message = "Le voyageur a été ajouté avec succès ";
+
+      return message;
+    } else if (response.statusCode == 400) {
+      var res = json.decode(response.body);
+      var message = "Le voyageur existe déja dans ce groupe";
+
+      return message;
+    } else {
+      throw Exception('Failed to load messages');
+    }
+  }
 }
