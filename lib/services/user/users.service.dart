71ad92e1d665ada23e054b451b57614ad232f3b1
nihelouanassi <nihelouanasi@gmail.com>
2023-12-01 19:09:49 +0100

    add search user by firstname service

--- lib/services/user/users.service.dart
+++ lib/services/user/users.service.dart
@@ -23,6 +23,25 @@ class UsersService {
     }
   }
 
+  Future<Users> getUsersWithConversation(String token, String userId) async {
+    final response = await http.get(
+      Uri.parse('${dotenv.env['API_URL']}/users/$userId/conversations'),
+      headers: <String, String>{
+        'Authorization': token,
+      },
+    );
+
+    if (response.statusCode == 200) {
+      var res = json.decode(response.body);
+      print("resssssssssssssusers conversation ");
+      print(res);
+
+      return Users.fromJson(res);
+    } else {
+      throw Exception('Failed to load user');
+    }
+  }
+
   Future<Users> getAllUsersBySerch(String token, String searchName) async {
     final response = await http.get(
       Uri.parse('${dotenv.env['API_URL']}/users/search/$searchName'),
